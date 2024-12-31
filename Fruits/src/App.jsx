aeeaa01c6db5145eeaf6250a41007d01c3dc40cf
deviradadiya <deviradadiya7@gmail.com>
2024-12-31 16:55:25 +0530

    add file

---
+++ Fruits/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './User';  // Import the UserList component
+
+function App() {
+  return (
+    <div className="App">
+      <User />  {/* Render the UserList component */}
+    </div>
+  );
+}
+
+export default App;
+
